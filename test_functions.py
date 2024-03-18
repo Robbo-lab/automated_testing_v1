7cf41f3963a4c530f61e9a8e6d1fbd6abe693e35
Robbozinoz <30688053+Robbozinoz@users.noreply.github.com>
2024-03-18 10:34:08 +0800

    feat: update test

--- test_functions.py
+++ test_functions.py
@@ -7,9 +7,11 @@ def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
 
+
 # uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+def test_subtract():
+   assert subtract(2, 3) == -1
+
 
 # uncomment the following test in step 11
 # def test_convert_fahrenheit_to_celsius():
